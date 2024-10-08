ac7a2f12e8d645da0ff8ec42d8f619ba902b52ac
Safin Singh <safin.singh@gmail.com>
2024-10-07 17:56:27 -0700

    feat: rudimentary elf reading

--- fesvr-rs/src/lib.rs
+++ fesvr-rs/src/lib.rs
@@ -1,4 +1,9 @@
-use std::time::Duration;
+use object::{
+    elf::{self, SHT_PROGBITS, SHT_SYMTAB},
+    read::elf::{FileHeader, SectionHeader, Sym},
+    Endianness,
+};
+use std::{error::Error, fs, path::Path, time::Duration};
 
 pub enum Syscall {
     Write,
@@ -34,34 +39,79 @@ impl Syscall {
     }
 }
 
+fn get_symbol_value(
+    symbols: &object::read::elf::SymbolTable<'_, elf::FileHeader64<object::Endianness>>,
+    symbol_name: &str,
+    endian: Endianness,
+) -> u64 {
+    symbols
+        .iter()
+        .find(|s| {
+            s.name(endian, symbols.strings())
+                .map(String::from_utf8_lossy)
+                .map_or(false, |s| s == "fromhost")
+        })
+        .expect(&format!("elf should have {}", symbol_name)) // clean up later
+        .st_value(endian)
+}
+
 trait Htif {
     const POLL_DELAY_MS: u64;
-
-    fn read(&self, ptr: usize, buf: &mut [u8]);
-    fn write(&self, ptr: usize, buf: &[u8]);
 
     // get from_host, to_host addresses
     fn from_host(&self) -> usize;
     fn to_host(&self) -> usize;
+
+    fn set_from_host(&self, ptr: usize);
+    fn set_to_host(&self, ptr: usize);
+
+    async fn read(&self, ptr: usize, buf: &mut [u8]);
+    async fn write(&self, ptr: usize, buf: &[u8]);
 
     async fn poll(&self) {
         let delay = Duration::from_millis(Self::POLL_DELAY_MS);
         loop {
             // dummy
             let mut buf = [0; 1];
-            self.read(self.to_host(), &mut buf);
+            self.read(self.to_host(), &mut buf).await;
 
             if let Ok(syscall) = Syscall::try_from(buf[0]) {
                 let res = syscall.execute();
                 let to_send = res.unwrap_or_else(|e| e as usize).to_ne_bytes(); // not sure if this is intended behavior
 
-                self.write(self.from_host(), &to_send);
+                self.write(self.from_host(), &to_send).await;
             } else {
                 println!("invalid syscall");
             }
 
             tokio::time::sleep(delay).await;
         }
+    }
+
+    async fn load_elf(&self, path: impl AsRef<Path>) -> Result<(), Box<dyn Error>> {
+        let data = fs::read(path)?;
+        let elf = elf::FileHeader64::<object::Endianness>::parse(&*data)?;
+        let endian = elf.endian()?;
+        let sections = elf.sections(endian, &*data)?;
+
+        for section in sections.iter() {
+            if section.sh_type(endian) == SHT_PROGBITS && section.sh_addr(endian) > 0 {
+                let data = section.data(endian, &*data)?;
+
+                // const CHUNK_SIZE: usize = 1024; do .chunks() for progress bar later
+                self.write(section.sh_addr(endian) as usize, &data).await;
+            }
+        }
+
+        let symbols = sections.symbols(endian, &*data, SHT_SYMTAB)?;
+
+        let from_host_addr = get_symbol_value(&symbols, "fromhost", endian) as usize;
+        let to_host_addr = get_symbol_value(&symbols, "tohost", endian) as usize;
+
+        self.set_from_host(from_host_addr);
+        self.set_from_host(to_host_addr);
+
+        Ok(())
     }
 }
 
