e1d4d91bd9116b56fda1f6326c8b1eb74f5eb436
AM <amaroo2006@gmail.com>
2025-04-11 11:39:50 -0700

    memory read optimizations. page_slice continues to be the bottleneck

--- sim/src/bus.rs
+++ sim/src/bus.rs
@@ -4,7 +4,7 @@ use log::info;
 use crate::cpu::{Exception, Result};
 use lazy_static::lazy_static;
 use std::{
-    cmp,
+    cmp::{self, Ordering},
     collections::{BTreeMap, HashMap},
     fmt::Debug,
     fs,
@@ -49,13 +49,13 @@ pub trait Device: Debug {
 
 #[derive(Debug)]
 pub struct Bus<'b> {
-    devices: BTreeMap<MemoryRange, Box<dyn Device + 'b>>,
+    devices: Vec<(MemoryRange, Box<dyn Device + 'b>)>,
 }
 
 impl<'b> Bus<'b> {
     pub fn new() -> Self {
         Self {
-            devices: BTreeMap::new(),
+            devices: Vec::new(),
         }
     }
 
@@ -64,22 +64,27 @@ impl<'b> Bus<'b> {
         addr: u64,
         len: u64,
     ) -> Result<(&MemoryRange, &mut (dyn Device + 'b))> {
-        self.devices
-            .range_mut(
-                ..=MemoryRange {
-                    base_address: addr,
-                    ..Default::default()
-                },
-            )
-            .rev()
-            .find(|(range, _)| range.contains(addr, len)) // should be first
-            .map(|(r, device)| (r, &mut **device))
-            .ok_or(Exception::LoadAccessFault)
+        match self.devices.binary_search_by(|(range, _)| {
+            if addr < range.base_address {
+                Ordering::Greater
+            } else if addr >= range.base_address + range.size {
+                Ordering::Less
+            } else {
+                Ordering::Equal
+            }
+        }) {
+            Ok(index) => {
+                let (range, device) = &mut self.devices[index];
+                Ok((range, &mut **device))
+            }
+            Err(_) => Err(Exception::LoadAccessFault),
+        }
     }
 
     pub fn register(&mut self, device: Box<dyn Device>, base_address: u64, size: u64) {
         self.devices
-            .insert(MemoryRange { base_address, size }, device);
+            .push((MemoryRange { base_address, size }, device));
+        self.devices.sort_by_key(|(range, _)| range.base_address);
     }
 }
 
@@ -116,16 +121,15 @@ pub struct Ram {
 impl Ram {
     pub const PAGE_SIZE: u64 = 0x1000;
     pub const PAGE_OFFSET_BITS: u64 = 12; // log(PAGE_SIZE)
+    pub const PAGE_MASK: u64 = (1 << Self::PAGE_OFFSET_BITS) - 1;
 }
-
-lazy_static! {
-    static ref ZERO_PAGE: Vec<u8> = vec![0u8; Ram::PAGE_SIZE as usize];
-}
-
 
 impl Ram {
     fn create_empty_page() -> Page {
-        Box::new([0u8; Ram::PAGE_SIZE as usize])
+        vec![0u8; Ram::PAGE_SIZE as usize]
+            .into_boxed_slice()
+            .try_into()
+            .unwrap_or_else(|_| panic!("Incorrect page size"))
     }
 
     fn page_slice(&mut self, ptr: u64, len: u64) -> &mut [u8] {
@@ -133,34 +137,43 @@ impl Ram {
             ptr >> Self::PAGE_OFFSET_BITS,
             ptr & ((1 << Self::PAGE_OFFSET_BITS) - 1),
         );
-    
-        let page = self.sparse_memory_map.entry(page_id)
-            .or_insert_with(Self::create_empty_page); 
-    
+
+        let page = self
+            .sparse_memory_map
+            .entry(page_id)
+            .or_insert_with(Self::create_empty_page);
+
         let start = page_offset as usize;
         let end = start + len as usize;
-    
-        debug_assert!(end <= Ram::PAGE_SIZE as usize); 
+
+        if end > Ram::PAGE_SIZE as usize {
+            panic!(
+                "page_slice out of bounds: offset {} + len {} > page size {}",
+                page_offset,
+                len,
+                Ram::PAGE_SIZE
+            );
+        }
         &mut page[start..end]
     }
 }
 
 impl Device for Ram {
     fn read(&mut self, ptr: u64, buf: &mut [u8]) -> Result<()> {
+        let mut offset = 0;
         let mut ptr = ptr;
-        let mut remaining = buf;
+        while offset < buf.len() {
+            let page_offset = (ptr & Self::PAGE_MASK) as usize;
+            let bytes_in_page = Self::PAGE_SIZE - page_offset as u64;
+            let to_read = cmp::min(buf.len() - offset, bytes_in_page as usize);
 
-        while !remaining.is_empty() {
-            let page_offset = ptr & ((1 << Self::PAGE_OFFSET_BITS) - 1);
+            let dst = &mut buf[offset..offset + to_read];
+            let src = &self.page_slice(ptr, to_read as u64)[..to_read];
 
-            let bytes_in_page = Self::PAGE_SIZE as usize - page_offset as usize;
-            let to_read = cmp::min(remaining.len(), bytes_in_page);
-            let (chunk, rest) = remaining.split_at_mut(to_read);
-
-            chunk.copy_from_slice(self.page_slice(ptr, to_read as u64));
+            dst.copy_from_slice(src);
 
             ptr += to_read as u64;
-            remaining = rest;
+            offset += to_read;
         }
 
         Ok(())
